dcbfcc369f2b851a516475f911b9014a95908c2a
dTap <imtapped@protonmail.com>
2022-11-03 21:30:49 +0700

    Added async fetch for try/catch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello my pretty!');
+let apiQuotes = [];
+
+
+// Get some Quotes from an API using an async fetch request within a try catch statement.
+// An asynchronous function can run at any time independently and won't stop the browser from completing the loading of the page
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {
+    // Catch the error
+  }
+}
+// On load
+getQuotes();
