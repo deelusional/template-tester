4002441275bca7fd3892293a0bb34ec7945365fe
dTap <imtapped@protonmail.com>
2022-11-04 18:44:18 +0700

    Commented out apiQuotes and replaced it with localQuotes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let apiQuotes = [];
+// let apiQuotes = [];
 
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   console.log(quote);
  }
 
 // Get some Quotes from an API using an async fetch request within a try catch statement.
 // An asynchronous function can run at any time independently and won't stop the browser from completing the loading of the page
-
-async function getQuotes() {
-  const apiUrl = 'https://type.fit/api/quotes';
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    // Catch the error
-  }
-}
-// On load
-getQuotes();
+// async function getQuotes() {
+//   const apiUrl = 'https://type.fit/api/quotes';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     // Catch the error
+//   }
+// }
+// // On load
+// getQuotes();
+newQuote();
