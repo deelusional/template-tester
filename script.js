0222e3721599f71608991391e7a99df5d65e13cd
dTap <imtapped@protonmail.com>
2022-11-04 18:21:11 +0700

    Added Math.floor

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let apiQuotes = [];
 
 
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+ }
+
 // Get some Quotes from an API using an async fetch request within a try catch statement.
 // An asynchronous function can run at any time independently and won't stop the browser from completing the loading of the page
 
@@ -9,7 +16,7 @@ async function getQuotes() {
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
+    newQuote();
   } catch (error) {
     // Catch the error
   }
