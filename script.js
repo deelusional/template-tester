11649a96d66939e2166d111ef78b66a9a9128139
dTap <imtapped@protonmail.com>
2022-11-06 20:55:01 +0700

    Changed apiUrl

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ function newQuote() {
 
 // Get some Quotes from an API using an async fetch request within a try catch statement.
 // An asynchronous function can run at any time independently and won't stop the browser from completing the loading of the page
-// async function getQuotes() {
-//   const apiUrl = 'https://type.fit/api/quotes';
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     // Catch the error
-//   }
-// }
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch the error
+  }
+}
 // // On load
 // getQuotes();
 newQuote();
