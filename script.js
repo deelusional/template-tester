6f74234df9722394d082ff52f25576c21a584fa1
dTap <imtapped@protonmail.com>
2022-11-07 22:14:05 +0700

    Check one two

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
 
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
  }
 
 // Get some Quotes from an API using an async fetch request within a try catch statement.
 // An asynchronous function can run at any time independently and won't stop the browser from completing the loading of the page
 async function getQuotes() {
-  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
